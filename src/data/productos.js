8b512f2d6ad356a62e2750102eb32b5ea3163cac
Matias Merlos <matiasjoaquinmerlos@gmail.com>
2024-08-29 06:58:58 -0300

    add: navbar, heroSection, productosSection, ok

---
+++ src/data/productos.js
@@ -0,0 +1,37 @@
+export const productos = [
+    {
+        id: 1,
+        aroma: "Rosas",
+        color: "Negro",
+        tamaño: "Mediana",
+        precio: 6000,
+    },
+    {
+        id: 2,
+        aroma: "Fresias",
+        color: "Blanco",
+        tamaño: "Pequeña",
+        precio: 4500,
+    },
+    {
+        id: 3,
+        aroma: "Sandía",
+        color: "Rosa",
+        tamaño: "Mediana",
+        precio: 6000,
+    },
+    {
+        id: 4,
+        aroma: "Limón",
+        color: "Negro",
+        tamaño: "Grande",
+        precio: 8500,
+    },
+    {
+        id: 5,
+        aroma: "Lavanda",
+        color: "Lila",
+        tamaño: "Pequeña",
+        precio: 4500,
+    }
+]
