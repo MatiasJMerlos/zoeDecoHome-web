fcf561e723041905f9f3814c0271cd89e400ae15
Matias Merlos <matiasjoaquinmerlos@gmail.com>
2024-09-20 08:17:26 -0300

    add: correciones en paleta de color y formulario

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,15 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors:{
+				claro:"#F2E3D5",
+				oscuro:"#40211A",
+				beige:"#D98F4E",
+				medioBajo:"#8C6954",
+				medioAlto:"#BFA995"
+			}
+		},
 	},
 	plugins: [require("@tailwindcss/forms")],
 }
