d04d536978f0d3d62198e51f11371d70fce33164
Matias Merlos <matiasjoaquinmerlos@gmail.com>
2024-10-06 20:14:31 -0300

    add: se agrega Login y Use

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -9,7 +9,34 @@ export default {
 				beige:"#D98F4E",
 				medioBajo:"#8C6954",
 				medioAlto:"#BFA995"
-			}
+			},
+
+			animation:{
+				fadeIn: "fadeIn",
+				fadeOut: "fadeOut",
+				scaleIn: "scaleIn",
+				scaleOut: "scaleOut",
+			},
+
+			keyframes:{
+				fadeIn:{
+					"0%": { opacity: 0 },
+					"100%": { opacity: 1 },
+				},
+				fadeOut:{
+					"0%": { opacity: 1 },
+					"100%": { opacity: 0 },
+				},
+				scaleIn:{
+					"0%": { transform: "scale(0)" },
+					"100%": { transform: "scale(1)" },
+				},
+				scaleOut:{
+					"0%": { transform: "scale(1)" },
+					"100%": { transform: "scale(0)" },
+				},
+			},
+
 		},
 	},
 	plugins: [require("@tailwindcss/forms")],
